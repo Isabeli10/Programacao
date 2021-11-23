536209680d03cc0f28bd551ecb22178bef59e516
Isabeli10 <89815367+Isabeli10@users.noreply.github.com>
2021-11-22 23:22:14 -0300

    Delete Exercicio9.py

--- Exercicio9.py
+++
@@ -1,8 +0,0 @@
-a = float(input("Digite o primeiro valor: "))
-b = float(input("Digite o segundo valor: "))
- 
-if a > b:
- print(f"O maior número é {a:.2f}")
-
-else:
- print(f"O maior número é {b:.2f}")
