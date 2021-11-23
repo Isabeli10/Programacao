72832e1167b2d5cd3a80392d46de7f1cb96d11b1
Isabeli10 <89815367+Isabeli10@users.noreply.github.com>
2021-11-22 23:21:59 -0300

    Delete Exercicio12.py

--- Exercicio12.py
+++
@@ -1,7 +0,0 @@
-base = float(input("Digite o valor da base do retângulo:"))
-altura = float(input("Digite o valor da altura do retângulo:"))
- 
-area = base * altura 
-
-if area > 100:
- print("Terreno Grande!")
