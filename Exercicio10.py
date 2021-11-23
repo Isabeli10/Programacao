cc08cc8be7e497f49cc1e09825994a616e58cff2
Isabeli10 <89815367+Isabeli10@users.noreply.github.com>
2021-11-22 23:21:44 -0300

    Delete Exercicio10.py

--- Exercicio10.py
+++
@@ -1,8 +0,0 @@
-a = float(input("Digite o primeiro valor: "))
-b = float(input("Digite o segundo valor: "))
- 
-if a < b:
- print(f"O menor número é {a:.2f}")
-
-else:
- print(f"O menor número é {b:.2f}")
