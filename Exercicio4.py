938d7c91f93fb2bc29d570fcff8e1751c1a07997
Isabeli10 <89815367+Isabeli10@users.noreply.github.com>
2021-11-23 00:14:40 -0300

    Delete Exercicio4.py

--- Exercicio4.py
+++
@@ -1,8 +0,0 @@
-primeiro = float(input("Digite o primeiro valor:"))
-segundo = float(input("Digite o segundo valor:"))
-terceiro = float(input("Digite o terceiro valor :"))
-quarto = float(input("Digite o quarto valor:"))
- 
-media = (primeiro+segundo+terceiro+quarto)/4
- 
-print(f"A média aritmetica dos números digitados é {media:.2f}")
