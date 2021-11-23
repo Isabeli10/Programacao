e2d86127fe4eb50b0ba08885ae906b06f4955c91
Isabeli10 <89815367+Isabeli10@users.noreply.github.com>
2021-11-22 23:12:16 -0300

    Delete Exercicio1.py

--- Exercicio1.py
+++
@@ -1,6 +0,0 @@
-base = float(input("Digite o valor da base do retângulo: "))
-altura = float(input("Digite o valor da altura do retângulo:"))
- 
-area = base * altura
- 
-print("A área do retângulo é ", area)
