11eb44ea11b459a5494ae8c2e6aabca413d2ae4a
Isabeli10 <89815367+Isabeli10@users.noreply.github.com>
2021-11-23 00:15:25 -0300

    Delete Exercicio6.py

--- Exercicio6.py
+++
@@ -1,7 +0,0 @@
-cotacao = float(input("Digite o valor da cotaçao do Dolar para Reais: "))
-dolar = float(input("Digite um valor em dolar: "))
-
- 
-reais = cotacao*dolar
- 
-print(f"O valor em Reais é R$ {reais:.2f} ")
